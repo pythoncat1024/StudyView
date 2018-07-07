30c4d7cff43a1eaa01252880c2b4bb501b887edb
pythonCat <pythoncat@qq.com>
2018-07-07 18:27:04 +0800

    add other code

---
+++ app/src/main/java/com/python/cat/studyview/OutActivity.java
@@ -0,0 +1,14 @@
+package com.python.cat.studyview;
+
+import android.os.Bundle;
+
+import com.python.cat.studyview.base.BaseActivity;
+
+public class OutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out);
+    }
+}
--- app/src/main/java/com/python/cat/studyview/RectActivity.java
+++ app/src/main/java/com/python/cat/studyview/RectActivity.java
@@ -1,6 +1,8 @@
 package com.python.cat.studyview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.apkfuns.logutils.LogUtils;
 import com.python.cat.studyview.base.BaseActivity;
@@ -12,5 +14,14 @@ public class RectActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rect);
         LogUtils.w("create...");
+
+        findViewById(R.id.btn_next)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        LogUtils.w("next click...");
+                        startActivity(new Intent(getActivity(), OutActivity.class));
+                    }
+                });
     }
 }
