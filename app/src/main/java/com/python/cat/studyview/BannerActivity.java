8315b0a8d40bb61923836efe55548a1f82a47ad8
pythonCat <pythoncat@qq.com>
2021-11-11 07:29:22 +0800

    无论是滑动结束，还是第一次显示，都是居中的了

--- app/src/main/java/com/python/cat/studyview/BannerActivity.java
+++ app/src/main/java/com/python/cat/studyview/BannerActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
 import androidx.viewpager.widget.ViewPager;
 
+import com.apkfuns.logutils.LogUtils;
 import com.python.cat.studyview.adapter.ItemAdapter;
 import com.python.cat.studyview.adapter.PageItemAdapter;
 import com.python.cat.studyview.adapter.transformer.Zoom;
@@ -33,16 +37,77 @@ public class BannerActivity extends BaseActivity {
         };
         List<String> stringList = Arrays.asList(display);
         RecyclerView rv = findViewById(R.id.rv);
-        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        new LinearSnapHelper().attachToRecyclerView(rv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rv.setLayoutManager(layoutManager);
+        LinearSnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(rv);
         rv.setAdapter(new ItemAdapter(stringList));
-        int pos = Integer.MAX_VALUE;
+        int pos = Integer.MAX_VALUE / 2;
         int rest = pos % stringList.size();
         pos -= rest;
         rv.scrollToPosition(pos);
+        rv.post(() -> {
+            int width = rv.getWidth();
+            View viewUnder = rv.findChildViewUnder(width / 2, rv.getHeight() / 2);
+            if (viewUnder != null) {
+                int cw = viewUnder.getWidth();
+                int x = (width - cw * 3) / 2;
+                LogUtils.d("x===" + x);
+                rv.scrollBy(-x, 0);
+            }
+        });
+        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                // dx>0则表示右滑, dx<0表示左滑, dy<0表示上滑, dy>0表示下滑
+                LogUtils.d("onScrolled: %s,%s", dx, dy);
+                int fp = layoutManager.findFirstVisibleItemPosition();
+                int lp = layoutManager.findLastVisibleItemPosition();
+                int middle = (int) (((long) lp + (long) fp) / 2L);
+                View view = layoutManager.findViewByPosition(middle);
+                LinearLayout ll = (LinearLayout) view;
+                TextView tv = ll.findViewById(R.id.mini_tv);
+                LogUtils.d("%d, %d, %d #text = %s", fp, lp, middle, tv.getText());
+                //                mCurrentItemOffset += dx;
+                //                computeCurrentItemPos();
+                //                onScrolledChangedCallback();
+            }
+        });
         extracted(stringList);
     }
 
+    /**
+     * RecyclerView位移事件监听, view大小随位移事件变化
+     */
+    //    private void onScrolledChangedCallback() {
+    //        int offset = mCurrentItemOffset - mCurrentItemPos * mOnePageWidth;
+    //        float percent = (float) Math.max(Math.abs(offset) * 1.0 / mOnePageWidth, 0.0001);
+    //
+    //        View leftView = null;
+    //        View currentView;
+    //        View rightView = null;
+    //        if (mCurrentItemPos > 0) {
+    //            leftView = mRecyclerView.getLayoutManager().findViewByPosition(mCurrentItemPos - 1);
+    //        }
+    //        currentView = mRecyclerView.getLayoutManager().findViewByPosition(mCurrentItemPos);
+    //        if (mCurrentItemPos < mRecyclerView.getAdapter().getItemCount() - 1) {
+    //            rightView = mRecyclerView.getLayoutManager().findViewByPosition(mCurrentItemPos + 1);
+    //        }
+    //
+    //        if (leftView != null) {
+    //            // y = (1 - mScale)x + mScale
+    //            leftView.setScaleY((1 - mScale) * percent + mScale);
+    //        }
+    //        if (currentView != null) {
+    //            // y = (mScale - 1)x + 1
+    //            currentView.setScaleY((mScale - 1) * percent + 1);
+    //        }
+    //        if (rightView != null) {
+    //            // y = (1 - mScale)x + mScale
+    //            rightView.setScaleY((1 - mScale) * percent + mScale);
+    //        }
+    //    }//
     private void extracted(List<String> stringList) {
         ViewPager vp = findViewById(R.id.pager);
         vp.setCurrentItem(stringList.size() / 2, false);
